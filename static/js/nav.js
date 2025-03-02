5792b754266d948a746887fe2cc988f935126743
Michael Adrian Villareal <michael.adrian.villareal@valmiz.com>
2025-03-02 14:39:09 +0800

    [top-level] fix menu bar mobile

--- static/js/nav.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('mobileMenuToggle').addEventListener('click', function() {
-    const navLinks = document.getElementById('navLinks');
-    navLinks.classList.toggle('active');
-    if (navLinks.classList.contains('active')) {
-      this.textContent = '✕';
-    } else {
-      this.textContent = '☰';
-    }
-  });
